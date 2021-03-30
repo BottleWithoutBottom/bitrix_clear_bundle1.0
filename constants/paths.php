dee73e770482dcfdb7b705bfcbad16e83fbc3e19
Juggerscript <shredder139@gmail.com>
2021-03-30 22:03:44 +0700

    *up: paths file refactored and updated

--- constants/paths.php
+++ constants/paths.php
@@ -1,11 +1,17 @@
 <?php
 define('LOCAL_PATH', $_SERVER['DOCUMENT_ROOT'] . '/local/');
-define('LOCAL_DIR_PATH', '/local/');
-define('DI_CONFIG_PATH', LOCAL_PATH . 'DI/config.php');
+define('LOCAL_DIR_PATH', SITE_DIR . 'local/');
+
+/** APP */
+define('APP_PATH', LOCAL_PATH . 'app/');
+define('APP_CONFIG_PATH', APP_PATH . 'config/');
+define('ROUTER_PATH', APP_PATH . 'Router/bootstrap.php');
+define('VIEWS_PATH', APP_PATH . 'Views/');
+define('DI_CONFIG_PATH', APP_CONFIG_PATH . 'DI.php');
+
+/** LOCAL */
 define('CONSTANTS_PATH', LOCAL_PATH . 'constants/');
 define('MODULES_PATH', LOCAL_PATH . 'modules/');
-define('LETSROCK_LIB_PATH', MODULES_PATH . 'letsrock.lib/lib/');
 define('AJAX_VIRTUAL_PATH', CONSTANTS_PATH . 'ajax-virtual.php');
-define('ROUTER_PATH', LETSROCK_LIB_PATH . 'router/bootstrap.php');
 define('INCLUDE_PATH', LOCAL_PATH . 'include/');
 define('COMMON_TEMPLATE_PATH', INCLUDE_PATH . 'templates/');
