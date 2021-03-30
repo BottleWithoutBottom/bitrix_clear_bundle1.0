a69ba879b443bb3941015a0e96278ee36180ada5
Leonid <shredder139@gmail.com>
2021-03-30 15:15:37 +0700

    *up: view added to the base controller

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -1,8 +1,30 @@
 <?php
 
-namespace App\Controllers;
+namespace Letsrock\Lib\Controllers;
+use Bitrix\Main\Application;
+use Letsrock\Lib\View\View;
 
-class Controller
+abstract class Controller
 {
+    protected $view;
 
+    public function __construct()
+    {
+        $this->view = new View();
+    }
+
+
+    public function getRequest()
+    {
+        return Application::getInstance()->getContext()->getRequest();
+    }
+
+    /**
+     * @param string $viewName - название view
+     * @param array $params - массив значений, которые будут преобразованы в переменные
+     */
+    public function render($viewName, $params = [])
+    {
+        return $this->view->render($viewName, $params = []);
+    }
 }
