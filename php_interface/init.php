004e5e9aab865409e23cb955375f58b58011a551
Juggerscript <shredder139@gmail.com>
2021-03-30 22:27:31 +0700

    *bugfix: init.php refactoring

--- php_interface/init.php
+++ php_interface/init.php
@@ -3,8 +3,6 @@ require($_SERVER['DOCUMENT_ROOT'] . '/local/constants/paths.php');
 require(AJAX_VIRTUAL_PATH);
 require(LOCAL_PATH . 'eventHandlers/eventHandlers.php');
 require(LOCAL_PATH . 'vendor/autoload.php');
-use Bitrix\Main\Loader;
-Loader::includeModule('letsrock.lib');
 
 function getContainer()
 {
