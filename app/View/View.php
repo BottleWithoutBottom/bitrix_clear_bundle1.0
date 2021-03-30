6fb5fb2db4c08f1cc063a716be5306c2ffcb83b0
Leonid <shredder139@gmail.com>
2021-03-30 15:15:24 +0700

    +up: basic View class and render method added

---
+++ app/View/View.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Letsrock\Lib\View;
+
+class View
+{
+    /**
+     * @param string $viewName - название view
+     * @param array $params - массив значений, которые будут преобразованы в переменные
+     */
+    public function render($viewName, $params = [])
+    {
+        if (empty($viewName)) return false;
+        if (!empty($params)) extract($params);
+        ob_start();
+        require(VIEWS_PATH . $viewName);
+        return ob_get_clean();
+    }
+}
