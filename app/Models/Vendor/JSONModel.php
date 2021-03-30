67db478a4619d8c0eaeda29a37f019dd87c6d41d
Leonid <shredder139@gmail.com>
2021-03-30 16:18:59 +0700

    +up: JSON model added

---
+++ app/Models/Vendor/JSONModel.php
@@ -0,0 +1,49 @@
+<?
+
+namespace App\Models\Vendor;
+
+class JSONModel extends Response
+{
+    private $status = false;
+    private $data = [];
+
+    /**
+     * @return mixed
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param mixed $status
+     */
+    public function setStatus($status): void
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    /**
+     * @param mixed $data
+     */
+    public function setData($data): void
+    {
+        $this->data = $data;
+    }
+
+    public function getAsString()
+    {
+        return json_encode([
+            'status' => $this->getStatus(),
+            'data' => $this->getData(),
+        ]);
+    }
+}
