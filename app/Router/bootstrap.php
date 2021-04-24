171d395cb466d3f0d0f6497058a6ababed015c60
Juggerscript <shredder139@gmail.com>
2021-04-24 17:17:20 +0700

    +up: all the constants included in bootstrap.php

--- app/Router/bootstrap.php
+++ app/Router/bootstrap.php
@@ -4,8 +4,9 @@
  * RewriteRule ^(.*)ajax-virtual(.*)$ /local/app/Router/bootstrap.php?$1 [QSA,L] - на апаче
  * rewrite ^(.*)ajax-virtual(.*)$ /local/app/Router/bootstrap.php?$1; - на Nginx
  */
-require('../vendor/autoload.php');
-
+require('./../vendor/autoload.php');
+require('./../constants/ajax-virtual.php');
+require('./../constants/paths.php');
 use App\Router\FastRouter;
 $router = FastRouter::getInstance();
 $router->handle();
